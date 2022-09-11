9c2dc14a77cc38f1005352b2af0c2d267ac908c6
duymoiz <minhduy12581@gmail.com>
2022-09-11 23:29:03 +0700

    second commit

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
 
-app.get('/tin-tuc', (req, res) => {
+app.use(morgan('combined'))
+
+app.get('/', (req, res) => {
     var a = 1;
     var b = 2;
 
     var c = a + b;
-    
+
   res.send('Hello World!')
 })
 
