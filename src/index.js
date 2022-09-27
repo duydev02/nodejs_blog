97acefe1c23d71872357e649ac229abfa4240997
duydev02 <minhduy12581@gmail.com>
2022-09-27 23:18:34 +0700

    Update

--- src/index.js
+++ src/index.js
@@ -3,22 +3,27 @@ const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
 const app = express();
-const port = 3000;
+const port = 8080;
 
 const route = require('./routes');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
 app.use(express.json());
 
 // HTTP logger
 // app.use(morgan('combined'));
 
 // Template engine
-app.engine('.hbs', handlebars.engine({ defaultLayout: 'main', extname: '.hbs' }));
+app.engine(
+    '.hbs',
+    handlebars.engine({ defaultLayout: 'main', extname: '.hbs' }),
+);
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
@@ -26,5 +31,5 @@ app.set('views', path.join(__dirname, 'resources/views'));
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
