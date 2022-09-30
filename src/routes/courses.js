a1e33bd99167d1110e1354ca975c8498b3d745a7
duydev02 <minhduy12581@gmail.com>
2022-09-30 18:20:38 +0700

    Func delete all

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -4,9 +4,10 @@ const router = express.Router();
 const courseController = require('../app/controllers/CourseController');
 
 router.get('/create', courseController.create);
+router.post('/store', courseController.store);
+router.post('/handle-form-actions', courseController.handleFormActions);
 router.get('/:id/edit', courseController.edit);
 router.get('/:slug', courseController.show);
-router.post('/store', courseController.store);
 router.patch('/:id/restore', courseController.restore);
 router.put('/:id', courseController.update);
 router.delete('/:id', courseController.destroy);
